5b5f1de7c31ad6230c03fbe021228672971477a9
Ivan Stoyanov <thraccoon04@gmail.com>
2025-09-07 11:10:06 +0300

    UI
    - Animated stats cards on click

--- Assets/UI/UIControllers/UpgradesUIController.cs
+++ Assets/UI/UIControllers/UpgradesUIController.cs
@@ -17,6 +17,12 @@ public class UpgradesUIController : IControllerUI
     private Button _nextButton;
 
     private bool _canRotateCards = true;
+    private bool _canClickCards = true;
+    private float _cardTransitionDuration = 0.2f;
+    private string[] properties;
+    private Scale _scaleUp = new Scale(new Vector2(1.1f, 1.1f));
+    private Color _originalCardColor = new Color(245f / 255f, 230f / 255f, 205f / 255f);
+    private Color _onClickCardColor = new Color(220f / 255f, 200f / 255f, 180f / 255f);
 
     public void Init(VisualElement root)
     {
@@ -31,9 +37,11 @@ public class UpgradesUIController : IControllerUI
         _rightCard = root.Q<VisualElement>("RightCard");
         _cards.Add(_rightCard);
 
+        properties = new string[] { "scale", "background-color" };
+
         foreach (VisualElement card in _cards)
         {
-            SetupCardsScaleTransition(card, "scale", 0.2f, EasingMode.Ease);
+            SetupCardsScaleTransition(card, properties, _cardTransitionDuration, EasingMode.Ease);
         }
 
         _closeButton = root.Q<Button>("CloseButton");
@@ -64,25 +72,37 @@ public class UpgradesUIController : IControllerUI
     }
 
 
-    public void OnLeftCard(ClickEvent evt)
+    public async void OnLeftCard(ClickEvent evt)
     {
+        if (!_canClickCards) return;
+
         AudioManager.Instance.PlayClickSound();
 
         Debug.Log("Left Card");
+
+        await ChangeCardColorOnClick(_leftCard);
     }
 
-    public void OnMiddleCard(ClickEvent evt)
+    public async void OnMiddleCard(ClickEvent evt)
     {
+        if (!_canClickCards) return;
+
         AudioManager.Instance.PlayClickSound();
 
         Debug.Log("Middle Card");
+
+        await ChangeCardColorOnClick(_middleCard);
     }
 
-    public void OnRightCard(ClickEvent evt)
+    public async void OnRightCard(ClickEvent evt)
     {
+        if (!_canClickCards) return;
+
         AudioManager.Instance.PlayClickSound();
 
         Debug.Log("Right Card");
+
+        await ChangeCardColorOnClick(_rightCard);
     }
 
 
@@ -96,7 +116,7 @@ public class UpgradesUIController : IControllerUI
     public async void OnPrev(ClickEvent evt)
     {
         if (!_canRotateCards) return;
-        
+
         AudioManager.Instance.PlayClickSound();
 
         await AnimateCardsRotation();
@@ -112,11 +132,11 @@ public class UpgradesUIController : IControllerUI
     }
 
 
-    private void SetupCardsScaleTransition(VisualElement element, string property, float duration, EasingMode easingMode)
+    private void SetupCardsScaleTransition(VisualElement element, string[] properties, float duration, EasingMode easingMode)
     {
-        element.style.transitionProperty = new List<StylePropertyName> { new StylePropertyName(property) };
-        element.style.transitionDuration = new List<TimeValue> { new TimeValue(duration, TimeUnit.Second) };
-        element.style.transitionTimingFunction = new List<EasingFunction> { new EasingFunction(easingMode) };
+        element.style.transitionProperty = new List<StylePropertyName> { new StylePropertyName(properties[0]), new StylePropertyName(properties[1]) };
+        element.style.transitionDuration = new List<TimeValue> { new TimeValue(duration, TimeUnit.Second), new TimeValue(duration, TimeUnit.Second) };
+        element.style.transitionTimingFunction = new List<EasingFunction> { new EasingFunction(easingMode), new EasingFunction(easingMode) };
     }
 
     private async Task AnimateCardsRotation()
@@ -128,11 +148,11 @@ public class UpgradesUIController : IControllerUI
         {
             if (card != null)
             {
-                card.style.scale = new Scale(new Vector2(1.1f, 1.1f));
+                card.style.scale = _scaleUp;
             }
         }
 
-        await Task.Delay(200);
+        await Task.Delay((int)(_cardTransitionDuration * 1000));
 
         foreach (var card in _cards)
         {
@@ -143,5 +163,19 @@ public class UpgradesUIController : IControllerUI
         }
 
         _canRotateCards = true;
+    }
+
+    private async Task ChangeCardColorOnClick(VisualElement element)
+    {
+        if (!_canClickCards) return;
+        _canClickCards = false;
+
+        element.style.backgroundColor = _onClickCardColor;
+
+        await Task.Delay((int)(_cardTransitionDuration * 1000));
+
+        element.style.backgroundColor = _originalCardColor;
+
+        _canClickCards = true;
     }
 }
